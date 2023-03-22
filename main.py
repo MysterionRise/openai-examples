a5e8b6b0ce40e7e57bcdad11644f9460ad7cb873
Konstantin Perikov <pericov.ko@gmail.com>
2023-03-22 01:32:37 +0000

    update api to gpt-4

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
-
 import openai
 
 
 def main():
-    print(do_action_as_chatgpt())
+    print(do_action_as_chatgpt(
+        "You're chatbot that answers questions from citizens about Ukraine constitution and other laws and regulations in Ukrainian language",
+        "Response must have links related to parts of the documents"
+        "Чи потрібно мені прописуватись при проживанні в дачному селищі?"
+    ))
+
 
 
 def get_available_models():
@@ -22,19 +26,25 @@ def generate_tweet(prompt: str = None):
     )
 
 
-def do_action_as_chatgpt(assistant_role: str = None, prompt: str = None):
+def do_action_as_chatgpt(assistant_role1: str = None, assistant_role2: str = None, prompt: str = None):
     openai.api_key_path = ".openai-api-key"
+    temperature = random.random()
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {"role": "system",
-             "content": "" if assistant_role is None else assistant_role},
+             "content": "" if assistant_role1 is None else assistant_role1},
+            {"role": "system",
+             "content": "" if assistant_role2 is None else assistant_role2},
             {"role": "user",
              "content": "" if prompt is None else prompt}
-        ]
+        ],
+        frequency_penalty=2,
+        temperature=temperature
     )
-
-    return completion.choices[0].message
+    print(f"temperature - {temperature}")
+    print(completion.choices[0].message["content"])
+    return ""
 
 
 if __name__ == '__main__':
